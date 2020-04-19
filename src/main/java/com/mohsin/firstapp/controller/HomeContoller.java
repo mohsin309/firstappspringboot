2417545de9ef79e1184abd0e407b1f9aeb819291
mohsin309 <alimohsin113245@gmail.com>
2020-04-19 19:27:43 +0500

    first commit

---
+++ src/main/java/com/mohsin/firstapp/controller/HomeContoller.java
@@ -0,0 +1,23 @@
+package com.mohsin.firstapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeContoller {
+    @GetMapping("/")
+    public String sayHelloGet(){
+        return "Hello world from get";
+    }
+    @PostMapping("/")
+    public String sayHelloPost(){
+        return "Hello world from post";
+    }
+    @PutMapping("/")
+    public String sayHello(){
+        return "Hello world from put";
+    }
+    @DeleteMapping("/")
+    public String sayHelloPutDelete(){
+        return "Hello world from delete";
+    }
+}
